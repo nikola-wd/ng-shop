bddf02b81a0a47b66eafee217ee4602bec404d58
nikola-wd <nikola_wd@hotmail.com>
2018-02-09 01:45:50 +0100

    added visibility toggle for filter

--- src/app/products/filters/filters.component.ts
+++ src/app/products/filters/filters.component.ts
@@ -8,16 +8,23 @@ import { ProductsService } from '../../services/products.service';
 })
 export class FiltersComponent implements OnInit {
   filterBy: string;
+  filterToggle: boolean;
 
   constructor(private prodService: ProductsService) { }
 
   ngOnInit() {
+    this.setFilterToggle();
     this.filterBy = this.prodService.getFilter();
     this.prodService.filterTypeEmitter.subscribe(
       (filverValue: string) => {
         this.filterBy = filverValue;
       }
     );
+  }
+
+
+  setFilterToggle() {
+    this.filterToggle = window.outerWidth > 768 ? true : false;
   }
 
 
