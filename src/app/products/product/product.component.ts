fa10612b569a9e476206ad3beeae2491b4ec3c51
nikola-wd <nikola_wd@hotmail.com>
2018-02-11 23:13:49 +0100

    added DoCheck hook on product single view to support changing from withing that view and refactored init code for that component

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Product } from '../product.model';
 import { ProductsService } from '../../services/products.service';
@@ -9,25 +9,34 @@ import { ProductsService } from '../../services/products.service';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, DoCheck {
+  id: number;
   product: Product;
-  // id: number;
   similarProducts: Product[];
 
   constructor(private route: ActivatedRoute, private prodService: ProductsService) { }
 
   ngOnInit() {
     // this.id = this.route.snapshot.params['id'];
-
     // this code should be used only in the case of route params should change programatically from
-    // within this component, if not, code above is enough
+    // within this component, if not, code above is enough (this is not the case now, since we change from similar products via routerLink)
     // this.route.params.subscribe(
-    //   (params: Params) => {
-    //     this.id = params['id'];
-    //   }
-    // );
+      //   (params: Params) => {
+        //     this.id = params['id'];
+        //   }
+        // );
+    this.initProductSingleView();
+  }
 
-    this.product = this.prodService.getSingleProduct(this.route.snapshot.params['id']);
+
+  ngDoCheck() {
+    this.initProductSingleView();
+  }
+
+  // helper fn to save repeating same code in init and doCheck hooks
+  initProductSingleView() {
+    this.id = this.route.snapshot.params['id'];
+    this.product = this.prodService.getSingleProduct(this.id);
     this.getSimilarProducts(this.product.type, this.product.id);
   }
 
