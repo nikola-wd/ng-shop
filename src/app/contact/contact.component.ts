2650f078fd887ab4f543de06209ff39f515203be
nikola-wd <nikola_wd@hotmail.com>
2018-02-12 18:15:12 +0100

    finished contact form - template driven approach

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  @ViewChild('f') contactForm: NgForm;
+
+  // to show data after submit
+  submitted = false;
 
   constructor() { }
 
-  ngOnInit() {
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    window.alert(
+      `Your name: ${form.value.name},
+      Your email: ${form.value.email},
+      Your message: ${form.value.message}`
+    );
+    this.contactForm.reset();
   }
 
 }
